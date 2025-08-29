4790509b7ae1e6a15db23637d0d40d4f260429fe
killasath <voxid59142@zaxcal.com>
2025-08-29 11:00:23 -1000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~57lUEDSR#QHOM3dQ11GfqB4EkG1YjvGIdBeqv2c8lltx-nSGXILw",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "27832877458, // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
